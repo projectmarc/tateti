93eb58c46f145b9750de6e128f0c784267e71a96
projectmarc <108161709+projectmarc@users.noreply.github.com>
2024-07-09 16:13:30 -0300

    printBoard function implemented

--- tateti.c
+++ tateti.c
@@ -1,4 +1,37 @@
+#include <stdio.h>
+
+char board[3][3];
+
+// Write all the cells of the board with ' '
+void initializeBoard();
+void printBoard();
+
 int main()
 {
+    initializeBoard();
+    printBoard();
     return 0;
+}
+
+void initializeBoard()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("[%c] ", board[i][j]);
+        }
+        printf("\n");
+    }
 }
