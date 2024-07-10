a975d386cf99fd8afafe5b398d450b953dbf5de8
projectmarc <108161709+projectmarc@users.noreply.github.com>
2024-07-10 03:28:47 -0300

    added userMovement()

--- tateti.c
+++ tateti.c
@@ -10,6 +10,7 @@ void printBoard();
 bool isCellAvailable(int row, int col);
 int countAvailableCells();
 void computerMovement();
+void userMovement();
 
 int main()
 {
@@ -76,6 +77,27 @@ void computerMovement()
         {
             board[randomRow][randomCol] = 'o';
             isMovementAssigned = true;
+        }
+    }
+}
+
+void userMovement()
+{
+    bool isMovementAssigned = false;
+    int row, col;
+    while (!isMovementAssigned)
+    {
+        scanf("%i %i", &row, &col);
+        row -= 1;
+        col -= 1;
+        if (isCellAvailable(row, col))
+        {
+            board[row][col] = 'x';
+            isMovementAssigned = true;
+        }
+        else
+        {
+            printf("Enter a valid and available cell index\n");
         }
     }
 }
